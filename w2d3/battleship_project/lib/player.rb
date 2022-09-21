9f26f3f56010f75f4f1690ab006118280840a31e
pko912 <kopeter912@gmail.com>
2022-09-21 19:27:00 -0400

    w2d3

---
+++ w2d3/battleship_project/lib/player.rb
@@ -0,0 +1,9 @@
+class Player
+
+    def get_move
+        print "should print 'enter a position with coordinates separated with a space like `4 7`'"
+        gets.chomp.split(" ").map(&:to_i)
+        
+    end
+
+end
